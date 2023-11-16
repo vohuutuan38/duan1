6815234fd12cb4bfa150a1e8e817073b8d3d288c
phonglenus <hongphongle115@gmail.com>
2023-11-16 22:25:37 +0700

    update db.sql

--- db.sql
+++ db.sql
@@ -1,12 +1,16 @@
 -- Bảng người dùng (Users)
-USE valiu;
-
 CREATE TABLE Users (
   user_id INT AUTO_INCREMENT PRIMARY KEY,
   username VARCHAR(255) NOT NULL,
   password VARCHAR(255) NOT NULL,
-  email VARCHAR(255) NOT NULL,
+  email VARCHAR(255) UNIQUE NOT NULL,
   user_type ENUM('admin', 'customer') NOT NULL
+);
+
+-- Bảng danh mục (Categories)
+CREATE TABLE Categories (
+  category_id INT AUTO_INCREMENT PRIMARY KEY,
+  category_name VARCHAR(255) NOT NULL
 );
 
 -- Bảng sản phẩm (Products)
@@ -16,14 +20,16 @@ CREATE TABLE Products (
   description TEXT,
   price DECIMAL(10, 2) NOT NULL,
   stock_quantity INT NOT NULL,
-  image_url VARCHAR(255)
+  image_url VARCHAR(255),
+  category_id INT,
+  FOREIGN KEY (category_id) REFERENCES Categories(category_id)
 );
 
 -- Bảng đơn hàng (Orders)
 CREATE TABLE Orders (
   order_id INT AUTO_INCREMENT PRIMARY KEY,
   user_id INT,
-  order_date DATE NOT NULL,
+  order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   status ENUM('pending', 'processing', 'shipped', 'canceled') NOT NULL,
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
@@ -57,16 +63,6 @@ CREATE TABLE ProductVariants (
   FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
--- Bảng lịch sử giá (PriceHistory)
-CREATE TABLE PriceHistory (
-  price_id INT AUTO_INCREMENT PRIMARY KEY,
-  product_id INT,
-  old_price DECIMAL(10, 2),
-  new_price DECIMAL(10, 2),
-  change_date DATE,
-  FOREIGN KEY (product_id) REFERENCES Products(product_id)
-);
-
 -- Bảng đánh giá và bình luận (ProductReviews)
 CREATE TABLE ProductReviews (
   review_id INT AUTO_INCREMENT PRIMARY KEY,
@@ -74,6 +70,7 @@ CREATE TABLE ProductReviews (
   user_id INT,
   rating INT NOT NULL,
   comment TEXT,
+  review_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (product_id) REFERENCES Products(product_id),
   FOREIGN KEY (user_id) REFERENCES Users(user_id)
 );
@@ -83,8 +80,8 @@ CREATE TABLE Promotions (
   promotion_id INT AUTO_INCREMENT PRIMARY KEY,
   product_id INT,
   discount_percentage DECIMAL(5, 2),
-  start_date DATE,
-  end_date DATE,
+  start_date TIMESTAMP,
+  end_date TIMESTAMP,
   FOREIGN KEY (product_id) REFERENCES Products(product_id)
 );
 
@@ -100,8 +97,7 @@ CREATE TABLE PaymentHistory (
   order_id INT,
   method_id INT,
   amount DECIMAL(10, 2) NOT NULL,
-  payment_date DATETIME,
+  payment_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
   FOREIGN KEY (order_id) REFERENCES Orders(order_id),
   FOREIGN KEY (method_id) REFERENCES PaymentMethods(method_id)
 );
-
